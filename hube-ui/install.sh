30fadb11784b69413da8042843ec80ef7808a8a8
Bogdan Dobrica <bdobrica@gmail.com>
2022-07-07 21:22:25 +0300

    Update install.sh

--- hube-ui/install.sh
+++ hube-ui/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# install worldpress
 curl -o wordpress.zip -L https://wordpress.org/latest.zip
 unzip wordpress.zip
 mv wordpress/* /var/www
@@ -10,6 +11,7 @@ rm -Rf /var/www/wp-content/plugins/akismet
 rm -Rf /var/www/wp-content/plugins/hello.php
 rm -Rf /var/www/wp-content/themes/twenty*
 
+# copy flat-ui files
 curl -o flatui.zip -L https://github.com/designmodo/Flat-UI/archive/master.zip
 unzip flatui.zip
 mkdir -p /var/www/wp-content/themes/hube-ui/assets/css/
@@ -22,4 +24,5 @@ sed -i '1 i\/*\nName:bootstrap\n*/' /var/www/wp-content/themes/hube-ui/assets/cs
 sed -i '1 i\/*\nName:flat-ui\nDependencies:jquery\nVersion:0.1\nFooter:true\n*/' /var/www/wp-content/themes/hube-ui/assets/js/flat-ui.min.js
 rm -Rf flatui.zip Flat-UI-master
 
-awk 'NR==FNR {a[NR]=$0;next} {print} /### SALT ###/ { for(i=1; i<=length(a); i++) {print a[i]}}' <( curl -L https://api.wordpress.org/secret-key/1.1/ ) /var/www/wp-config.php > /var/www/wp-config.php
+# create wp-config.php
+sed -i '/^### SALT ###$/r'<( curl -L https://api.wordpress.org/secret-key/1.1/ ) /var/www/wp-config.php
