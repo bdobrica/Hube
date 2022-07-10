0002dc04465020b37dd3fc308114cc60b7d5fee0
Bogdan Dobrica <bdobrica@gmail.com>
2022-07-10 15:14:16 +0300

    Update install.sh

--- hube-ui/install.sh
+++ hube-ui/install.sh
@@ -19,6 +19,7 @@ mkdir -p /var/www/wp-content/themes/hube-ui/assets/js/
 cp Flat-UI-master/dist/css/flat-ui.min.css /var/www/wp-content/themes/hube-ui/assets/css/flat-ui.min.css
 cp Flat-UI-master/dist/css/vendor/bootstrap.min.css /var/www/wp-content/themes/hube-ui/assets/css/bootstrap.min.css
 cp Flat-UI-master/dist/scripts/flat-ui.min.js /var/www/wp-content/themes/hube-ui/assets/js/flat-ui.min.js
+cp -r Flat-UI-master/dist/fonts /var/www/wp-content/themes/hube-ui/assets/fonts
 sed -i '1 i\/*\nName:flat-ui\nDependencies:bootstrap\n*/' /var/www/wp-content/themes/hube-ui/assets/css/flat-ui.min.css
 sed -i '1 i\/*\nName:bootstrap\n*/' /var/www/wp-content/themes/hube-ui/assets/css/bootstrap.min.css
 sed -i '1 i\/*\nName:flat-ui\nDependencies:jquery\nVersion:0.1\nFooter:true\n*/' /var/www/wp-content/themes/hube-ui/assets/js/flat-ui.min.js
