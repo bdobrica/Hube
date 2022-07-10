9e5a708044350c0a77bb876952c926bf04bae07e
Bogdan Dobrica <bdobrica@gmail.com>
2022-07-10 14:29:42 +0300

    Update dashboard.php

--- hube-ui/hube-ui/roles/default/pages/dashboard.php
+++ hube-ui/hube-ui/roles/default/pages/dashboard.php
@@ -1,11 +1,10 @@
 <?php
-$hube_language = new HUBE_Language ();
-$languages = $hube_language->get ('languages');
-
-if (strpos ($_SERVER['REQUEST_URI'], '/admin/') !== FALSE) : ?>
+$hube_language = new HUBE_Language();
+$languages = $hube_language->get( 'languages' );
+?>
 <div class="row">
     <div class="col-lg-4 col-md-6 col-sm-12">
-        <a href="" class="hube-logo"><span><?php wp_title (); ?></span></a>
+        <a href="" class="hube-logo"><span><?php wp_title(); ?></span></a>
         <div class="hube-rounded hube-translucent hube-padded hube-center hube-trainer-login">
             <h5><?php HUBE_Theme::_e( /*T[*/'Trainer Login'/*]*/ ); ?></h5>
             <form action="" method="post">
@@ -14,7 +13,7 @@ if (strpos ($_SERVER['REQUEST_URI'], '/admin/') !== FALSE) : ?>
                 <label><?php HUBE_Theme::_e( /*T[*/'Password:'/*]*/ ); ?></label>
                 <input class="form-control" name="password" type="password" />
                 <label><?php HUBE_Theme::_e( /*T[*/'Language'/*]*/ ); ?>:</label>
-                <?php HUBE_Theme::inp ('locale', '', 'select', $languages); ?>
+                <?php HUBE_Theme::inp( 'locale', '', 'select', $languages ); ?>
                 <br />
                 <br />
                 <button class="btn btn-block btn-success"><?php HUBE_Theme::_e( /*T[*/'Login &raquo;'/*]*/ ); ?></button>
@@ -26,24 +25,3 @@ if (strpos ($_SERVER['REQUEST_URI'], '/admin/') !== FALSE) : ?>
         </div>
     </div>
 </div>
-<?php else : ?>
-<div class="row">
-    <div class="col-lg-4 col-md-6 col-sm-12">
-        <a href="" class="hube-logo"><span><?php wp_title (); ?></span></a>
-        <div class="hube-rounded hube-translucent hube-padded hube-center hube-player-login">
-            <h5><?php HUBE_Theme::_e( /*T[*/'Player Login'/*]*/ ); ?></h5>
-            <form action="" method="post">
-                <label><?php HUBE_Theme::_e( /*T[*/'Team Password'/*]*/ ); ?>:</label>
-                <input class="form-control" name="teampass" type="text" value="" placeholder="" />
-<?php if (FALSE) : ?>
-                <label><?php HUBE_Theme::_e( /*T[*/'Language'/*]*/ ); ?>:</label>
-                <?php HUBE_Theme::inp ('locale', '', 'select', $languages); ?>
-<?php endif; ?>
-                <br />
-                <button class="btn btn-block btn-success"><?php HUBE_Theme::_e( /*T[*/'Login &raquo;'/*]*/ ); ?></button>
-            </form>
-            <ul></ul>
-        </div>
-    </div>
-</div>
-<?php endif;
